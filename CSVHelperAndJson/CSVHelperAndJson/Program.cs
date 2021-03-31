78427d4b675eb773d5cccf8825b343f49db3afdf
prashikjaware <prashikjaware123@gmail.com>
2021-03-31 18:48:10 +0530

    [Prashik]Added: Class Problem Demo on CSV and Json

---
+++ CSVHelperAndJson/CSVHelperAndJson/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVHelperAndJson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //CSVHandler.ImplementCSVDataHandling();
+            ReadCSV_And_WriteJSON.ImplementCSVToJSON();
+            Console.ReadKey();
+        }
+    }
+}
